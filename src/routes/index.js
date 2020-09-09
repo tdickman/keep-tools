8f6e2b00ac90ec924d2dba2796f8d8287a28c1d2
Tom Dickman <tdickman@gmail.com>
2020-09-09 19:31:50 -0400

    Add message specifying required metamask network

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import { HashRouter } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import { SUPPORTED_NETWORKS } from '../config'
 
 export default function Routes() {
   const context = useWeb3Context();
@@ -18,10 +19,21 @@ export default function Routes() {
       <br/>
       {!context.active && (
       <Container>
-        <Button variant="primary" onClick={() => context.setConnector('Metamask')}>
-            Connect with Metamask
-        </Button>
+        <div style={{
+                  position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+              }}>
+          <div style={{width: '50%', margin: 'auto', 'padding-bottom': '10px'}}>
+            <Button variant="primary" onClick={() => context.setConnector('Metamask')}>
+              Connect with Metamask
+            </Button>
+          </div>
+          <p>Please make sure you have metamask installed and set to the ropsten network</p>
+        </div>
       </Container>)}
+      {context.active && SUPPORTED_NETWORKS.indexOf(context.networkId) === -1 &&
+        <p>Make sure you are on ropsten network</p>
+      }
       {context.active && <HashRouter>
         <Route path="/" exact component={Initialize} />
         <Route path="/random-beacon/entries" component={RandomBeaconEntries} />
